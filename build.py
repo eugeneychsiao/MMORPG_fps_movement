4daa6fd5539ef0562215421b6f3d127fc2319344
Eugene Hsiao <eugeneychsiao@gmail.com>
2022-05-11 18:40:52 -0400

    Update build.py

--- build.py
+++ build.py
@@ -1,10 +1,7 @@
-# Run with: python3 build.py
 import os
 import platform
-
 # (1)==================== COMMON CONFIGURATION OPTIONS ======================= #
 COMPILER="g++ -std=c++17"   # The compiler we want to use 
-                                #(You may try g++ if you have trouble)
 SOURCE="./src/*.cpp"    # Where the source code lives
 EXECUTABLE="lab"        # Name of the final executable
 # ======================= COMMON CONFIGURATION OPTIONS ======================= #
@@ -32,10 +29,7 @@ elif platform.system()=="Windows":
 # (2)=================== Platform specific configuration ===================== #
 
 # (3)====================== Building the Executable ========================== #
-# Build a string of our compile commands that we run in the terminal
 compileString=COMPILER+" "+ARGUMENTS+" -o "+EXECUTABLE+" "+" "+INCLUDE_DIR+" "+SOURCE+" "+LIBRARIES
-# Print out the compile string
-# This is the command you can type
 print("============v (Command running on terminal) v===========================")
 print("Compilng on: "+platform.system())
 print(compileString)
@@ -43,16 +37,3 @@ print("========================================================================"
 # Run our command
 os.system(compileString)
 # ========================= Building the Executable ========================== #
-
-
-# Why am I not using Make?
-# 1.)   I want total control over the system. 
-#       Occassionally I want to have some logic
-#       in my compilation process (like searching for missing files).
-# 2.)   Realistically our projects are 'small' enough 
-#       this will not matter.
-# 3.)   Feel free to implement your own make files or autogenerate it from this
-#       script
-# 4.)   It is handy to know Python
-
-
